2a16da523bfb9da7d9e6f4112a128b46bc9021fa
Avneet Singh <avneet723@gmail.com>
2017-07-16 13:47:40 -0700

    Iteration 2 with updated Hyper parameters

--- command.sh
+++ command.sh
@@ -1,4 +1,3 @@
-pip install -r floyd_requirements.txt
 cp -r /code/* /output
 cd /output
-/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/rUrpT8uHTBFzMhjYTGiuHZ' --NotebookApp.token='' --NotebookApp.allow_origin='*'
+/run_jupyter.sh --no-browser --NotebookApp.base_url='/notebooks/RJqw4JwWQgPZ6FdnnDxLVX' --NotebookApp.token='' --NotebookApp.allow_origin='*'
